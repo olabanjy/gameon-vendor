c9801bb653c304fe2c3e4f34168aa6bb6aebf728
Olabanji <shola.albert@gmail.com>
2022-08-24 16:14:42 +0100

    fixes

--- vendor/settings/dev.py
+++ vendor/settings/dev.py
@@ -53,7 +53,7 @@ DATABASES = {
 
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.zoho.com"
+EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
